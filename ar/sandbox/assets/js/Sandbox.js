583031446b6a1e43b792ff2a9b5c5ee6667ad970
charline <charlinelaportefernandez@gmail.com>
2018-10-07 21:45:24 +0200

    position de la grille

--- ar/sandbox/assets/js/Sandbox.js
+++ ar/sandbox/assets/js/Sandbox.js
@@ -159,8 +159,8 @@ class Sandboxe {
                 let material = new THREE.MeshBasicMaterial({color: t.colors.white, wireframe: true})
                 let mesh = new THREE.Mesh(geometry, material)
 
-                mesh.position.x = i + 1 - (t.gridSize / 2)
-                mesh.position.z = j + 1 - (t.gridSize / 2)
+                mesh.position.x = i + 1 - t.gridSize
+                mesh.position.z = j + 1 - t.gridSize
 
                 // ajoute à notre groupe
                 grid.add(mesh)
