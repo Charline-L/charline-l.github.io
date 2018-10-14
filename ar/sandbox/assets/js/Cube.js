e7b3b2433fe2f1c1a855928b58a582c6bc75f09d
charline <charlinelaportefernandez@gmail.com>
2018-10-14 13:48:46 +0200

    Ajout console.log fix du nom du cube

--- ar/sandbox/assets/js/Cube.js
+++ ar/sandbox/assets/js/Cube.js
@@ -2,10 +2,14 @@ class Cube {
     constructor(infos, scene) {
         const t = this
 
+        alert("in cube")
+
         // infomations de positions du cube
         t.x = infos.position.x
         t.y = infos.position.y
         t.z = infos.position.z
+
+        t.id = infos._id
 
         t.color = infos.color
         t.alpha = infos.alpha
@@ -35,18 +39,24 @@ class Cube {
     appendCube() {
         const t = this
 
+        // récupère notre grille dans notre scene
         let grid = t.scene.getObjectByName('grid')
 
+        console.log("grid", grid)
+
+        // défini la forme / texture du cube
         let geometry = new THREE.BoxGeometry(t.sizeCube, t.sizeCube, t.sizeCube)
         let material = new THREE.MeshBasicMaterial({color: t.colors.white, wireframe: true})
         let mesh = new THREE.Mesh(geometry, material)
 
+        // positionne le cube
         mesh.position.x = (t.x * t.sizeCube) - ((t.gridSize - 1) / 2 * t.sizeCube)
         mesh.position.z = (t.z * t.sizeCube) - ((t.gridSize - 1) / 2 * t.sizeCube)
 
-        mesh.name = 'mesh-' + count++
+        // donner nom unique au cube
+        mesh.name = t.id
 
-        // ajoute à notre groupe
+        // ajoute à notre groupe qui va l'ajouter à la scène
         grid.add(mesh)
     }
 }
