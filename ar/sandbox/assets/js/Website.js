3522b7ea9b62d169dd710598e9a84265caec159a
charline <charlinelaportefernandez@gmail.com>
2018-10-22 14:41:48 +0200

    Remet code hiro

--- ar/sandbox/assets/js/Website.js
+++ ar/sandbox/assets/js/Website.js
@@ -22,7 +22,7 @@ class Website {
             'patt.letterB',
         ]
 
-        new Sandboxe({pattern: 'patt.charline', id: 'ar-sandbox'})
+        new Sandboxe({pattern: 'patt.hiro', id: 'ar-sandbox'})
         // for (let pattern of patterns)  new Sandboxe(pattern)
     }
 
