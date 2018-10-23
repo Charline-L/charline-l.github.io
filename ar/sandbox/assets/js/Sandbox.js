42ec5e8f9f2d3410046affc3b1585ee5ff4bbe5a
charline <charlinelaportefernandez@gmail.com>
2018-10-24 01:28:11 +0200

    TEST: shadow sur la grille

--- ar/sandbox/assets/js/Sandbox.js
+++ ar/sandbox/assets/js/Sandbox.js
@@ -104,8 +104,8 @@ class Sandboxe {
         alert("9")
         t.directionalLight = new THREE.DirectionalLight( 'white' )
         // t.directionalLight.position.set( 1, 0, t.gridSize ).setLength(2)
-        t.directionalLight.position.set( 1, 0.5, 0 ).setLength(1)
-        t.directionalLight.shadow.mapSize.set(28,28)
+        t.directionalLight.position.set( 10, 5, 0 ).setLength(1)
+        t.directionalLight.shadow.mapSize.set(128,128)
         t.directionalLight.shadow.camera.bottom = -0.6
         t.directionalLight.shadow.camera.top = 0.6
         t.directionalLight.shadow.camera.right = 0.6
