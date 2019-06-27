64ba164b59e974850d29af0a718a52b668362ef8
charline <charlinelaportefernandez@gmail.com>
2019-06-27 16:13:39 +0200

    FIX: ajoute la demande de micro une fois le son passé + alert

--- assets/script/app.min-withnode.js
+++ assets/script/app.min-withnode.js
@@ -1657,25 +1657,8 @@ class app {
 
     init() {
 
+        alert('ok micro')
         this.detectPage()
-        this.toggleFullScreen()
-    }
-
-    toggleFullScreen() {
-        var doc = window.document;
-        var docEl = doc.documentElement;
-
-        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen;
-        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen;
-
-        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement) {
-            console.log('in if')
-            requestFullScreen.call(docEl);
-        }
-        else {
-            console.log('in else')
-            cancelFullScreen.call(doc);
-        }
     }
 
     detectPage() {
